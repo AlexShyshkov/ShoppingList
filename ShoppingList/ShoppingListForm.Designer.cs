c7f71723cd3a0deee04510b7cd35c792c3cc0674
Alex Shyshkov <shi-alex@ukr.net>
2018-06-09 20:46:12 +0300

    1

---
+++ ShoppingList/ShoppingListForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ShoppingList
+{
+    partial class ShoppingListForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.ItemTextBox = new System.Windows.Forms.TextBox();
+            this.ItemCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAdd.Enabled = false;
+            this.buttonAdd.Location = new System.Drawing.Point(210, 243);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDelete.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonDelete.Enabled = false;
+            this.buttonDelete.Location = new System.Drawing.Point(12, 272);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(275, 23);
+            this.buttonDelete.TabIndex = 1;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // ItemTextBox
+            // 
+            this.ItemTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ItemTextBox.Location = new System.Drawing.Point(12, 243);
+            this.ItemTextBox.Name = "ItemTextBox";
+            this.ItemTextBox.Size = new System.Drawing.Size(180, 20);
+            this.ItemTextBox.TabIndex = 2;
+            this.ItemTextBox.TextChanged += new System.EventHandler(this.ItemTextBox_TextChanged);
+            // 
+            // ItemCheckedListBox
+            // 
+            this.ItemCheckedListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ItemCheckedListBox.CheckOnClick = true;
+            this.ItemCheckedListBox.FormattingEnabled = true;
+            this.ItemCheckedListBox.Location = new System.Drawing.Point(12, 11);
+            this.ItemCheckedListBox.Name = "ItemCheckedListBox";
+            this.ItemCheckedListBox.Size = new System.Drawing.Size(275, 214);
+            this.ItemCheckedListBox.TabIndex = 3;
+            this.ItemCheckedListBox.SelectedIndexChanged += new System.EventHandler(this.ItemCheckedListBox_SelectedIndexChanged);
+            // 
+            // ShoppingListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 307);
+            this.Controls.Add(this.ItemCheckedListBox);
+            this.Controls.Add(this.ItemTextBox);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Name = "ShoppingListForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shopping List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.TextBox ItemTextBox;
+        private System.Windows.Forms.CheckedListBox ItemCheckedListBox;
+    }
+}
+
---
+++ ShoppingList/ShoppingListForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ShoppingList
+{
+    public partial class ShoppingListForm : Form
+    {
+        private string item = "";
+
+        public ShoppingListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            ItemCheckedListBox.Items.Add(item);
+            buttonAdd.Enabled = false;
+            ItemTextBox.Clear();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            ItemCheckedListBox.Items.RemoveAt(ItemCheckedListBox.SelectedIndex);
+            buttonDelete.Enabled = false;
+        }
+
+        private bool DeleteSpacebar
+        {
+            get
+            {
+                char[] symbol = { ' ' };
+                string[] words = ItemTextBox.Text.Split(symbol, StringSplitOptions.RemoveEmptyEntries);
+                item = string.Join(" ", words);
+                return true;
+            }
+        }
+
+        private bool IsNotEqualItem
+        {
+            get
+            {
+                bool isSpscebar = DeleteSpacebar;
+                return item.Length > 0 && ItemCheckedListBox.FindStringExact(item) == -1;
+            }
+        }
+
+        private void ItemCheckedListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            buttonDelete.Enabled = true;
+        }
+
+        private void ItemTextBox_TextChanged(object sender, EventArgs e)
+        {
+            buttonAdd.Enabled = IsNotEqualItem;
+        }
+    }
+}
